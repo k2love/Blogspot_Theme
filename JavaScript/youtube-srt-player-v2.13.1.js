b23458b4feeed0c3500af771719ab2c2dfb23313
k2love <42851173+k2love@users.noreply.github.com>
2025-01-24 17:36:26 +0900

    Update youtube-srt-player-v2.13.1.js

--- JavaScript/youtube-srt-player-v2.13.1.js
+++ JavaScript/youtube-srt-player-v2.13.1.js
@@ -368,7 +368,7 @@
     }
 
     .subtitle-area {
-        position: relative;
+        position: absolute;
         width: 100%;
         height: 40px;
         background: #000;
@@ -376,7 +376,8 @@
         align-items: center;
         justify-content: center;
         text-align: center;
-        z-index: 10;
+        z-index: 100;
+        pointer-events: none;
     }
 
     .subtitle-area > div {
